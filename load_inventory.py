43e8d2649821e95b25a099bfa751b375ab23a445
Nimna Devinda <102314757+N-Devin@users.noreply.github.com>
2024-03-18 05:29:03 +0000

    python files

--- load_inventory.py
+++ load_inventory.py
@@ -1,38 +1,47 @@
 from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
+from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.vars.manager import VariableManager
+from ansible.plugins.callback import CallbackBase
+
+
+class ResultCallback(CallbackBase):
+    def v2_runner_on_ok(self, result, **kwargs):
+        host = result._host.get_name()
+        ip = result._host.get_vars().get('ansible_host', 'N/A')
+        groups = ', '.join(result._host.get_groups())
+        print(f"Host: {host}, IP: {ip}, Groups: {groups}")
+        print(f"Ping result: {result._result.get('ping')}")
 
 
 def load_inventory(inventory_file='hosts.yml'):
-    # Initialize Ansible DataLoader
     loader = DataLoader()
-
-    # Load inventory file
     inventory = InventoryManager(loader=loader, sources=inventory_file)
-
     return inventory
 
 
-def print_host_info(host):
-    name = host.name
-    ip = host.vars.get('ansible_host', 'N/A')
-    groups = ', '.join(group.name for group in host.groups)
-    print(f"Host: {name}, IP: {ip}, Groups: {groups}")
-
-
 def ping_all_hosts(inventory):
-    print("\nPing Results:")
-    for host in inventory.get_hosts():
-        result = host.get_vars().get('_ansible_parsed', {}).get('ping', {})
-        ping_status = result.get('ping', 'Failed')
-        print(f"  {host.name}: {ping_status}")
+    results_callback = ResultCallback()
+    variable_manager = VariableManager(
+        loader=inventory._loader, inventory=inventory)
+    task_queue_manager = TaskQueueManager(
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=inventory._loader,
+        passwords={},
+        stdout_callback=results_callback,
+    )
+    ping_task = dict(
+        name="Ping all hosts",
+        action=dict(module="ping", args=""),
+        hosts='all',
+        roles=['common']
+    )
+    task_queue_manager.run([ping_task])
 
 
 if __name__ == "__main__":
     inventory_file = 'hosts.yml'
     inventory = load_inventory(inventory_file)
-
-    print("Hosts:")
-    for host in inventory.get_hosts():
-        print_host_info(host)
-
+    print("Hosts and Ping Results:")
     ping_all_hosts(inventory)
--- run_playbook.py
+++ run_playbook.py
@@ -4,44 +4,31 @@ from ansible.parsing.dataloader import DataLoader
 from ansible.vars.manager import VariableManager
 
 
-def load_inventory(inventory_file='hosts.yml', private_key_file='./secrets/id_rsa'):
-    # Initialize Ansible DataLoader
+def load_inventory(inventory_file='hosts.yml'):
     loader = DataLoader()
-
-    # Load inventory file
     inventory = InventoryManager(loader=loader, sources=inventory_file)
-
-    # Initialize VariableManager
     variable_manager = VariableManager(loader=loader, inventory=inventory)
-
-    # Set private key file
-    if private_key_file:
-        variable_manager.extra_vars = {
-            'ansible_ssh_private_key_file': private_key_file}
-
     return inventory, variable_manager
+
+
+def run_playbook(playbook_path, inventory):
+    loader = DataLoader()
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    passwords = {}
+
+    playbook_executor = PlaybookExecutor(
+        playbooks=[playbook_path],
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        passwords=passwords
+    )
+    result = playbook_executor.run()
+    print(result)
 
 
 if __name__ == "__main__":
     playbook_path = 'hello.yml'
     inventory_file = 'hosts.yml'
-    private_key_file = './secrets/id_rsa'
-
-    # Load inventory and variable manager
-    inventory, variable_manager = load_inventory(
-        inventory_file, private_key_file)
-
-    # Initialize Ansible DataLoader
-    loader = DataLoader()
-
-    # Run playbook
-    executor = PlaybookExecutor(
-        playbooks=[playbook_path],
-        inventory=inventory,
-        variable_manager=variable_manager,
-        loader=loader,
-        passwords={},
-    )
-
-    results = executor.run()
-    print(results)
+    inventory, _ = load_inventory(inventory_file)
+    run_playbook(playbook_path, inventory)
